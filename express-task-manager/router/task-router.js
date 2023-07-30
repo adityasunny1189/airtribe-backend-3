4a5ae51bd68536dc8e60c2257514b4c91393d8df
Aditya Pathak <adityapathak1189@gmail.com>
2023-07-30 20:05:18 +0530

    post and delete

--- express-task-manager/router/task-router.js
+++ express-task-manager/router/task-router.js
@@ -5,6 +5,7 @@ const taskValidator = require('../utils/validators/task-validator');
 
 const validator = new taskValidator;
 
+// [Status: ✅]
 taskRouter.get('/', (req, res) => {
     res.status(200).json(tasksData);
 })
@@ -12,24 +13,44 @@ taskRouter.get('/', (req, res) => {
 taskRouter.get('/:id', (req, res) => {
     const id = req.params.id;
     if(!validator.checkId(id)) {
-        res.json({
+        res.status(400).json({
             "message": "Id can't be negative"
         });
-    }
-    const task = tasksData.filter((task) => task.id == id);
-    if(task.length > 0) {
-        res.status(200).json(task);
     } else {
-        res.status(404).json({
-            "message": "Task not found for this ID"
-        })
+        const task = tasksData.filter((task) => task.id == id);
+        if(task !== undefined || task.length !== 0) {
+            res.status(200).json(task);
+        } else {
+            res.status(404).json({
+                "message": "Task not found for this ID"
+            })
+        }
     }
 })
 
+// [Status: ✅]
 taskRouter.post('/', (req, res) => {
-    const reqData = req.body;
-    console.log(reqData);
-    res.status(201).send('received');
+    const newTask = req.body;
+    if(!validator.checkTaskReqBody(newTask)) {
+        res.status(400).json({
+            "message": "Invalid task body",
+            "task": newTask
+        });
+    } else {
+        if(!validator.checkTaskDuplicateId(newTask, tasksData)) {
+            res.status(400).json({
+                "message": "Task id already present",
+                "task": newTask
+            });
+        } else {
+            tasksData.push(newTask);
+            fs.writeFileSync('./data.json', JSON.stringify(tasksData));
+            res.status(201).json({
+                "message": "Task created successfully",
+                "task": newTask
+            });
+        }
+    }
 })
 
 taskRouter.put('/:id', (req, res) => {
@@ -37,7 +58,26 @@ taskRouter.put('/:id', (req, res) => {
 })
 
 taskRouter.delete('/:id', (req, res) => {
-
+    const id = req.params.id;
+    if(!validator.checkId(id)) {
+        res.status(400).json({
+            "message": "Id can't be negative"
+        })
+    } else {
+        const task = tasksData.filter((task) => task.id == id);
+        if(task !== undefined || task.length !== 0) {
+            const taskIndex = tasksData.indexOf(task[0]);
+            tasksData.splice(taskIndex, 1);
+            fs.writeFileSync('./data.json', JSON.stringify(tasksData));
+            res.status(204).json({
+                "message": "Task deleted successfully"
+            })
+        } else {
+            res.status(404).json({
+                "message": "Invalid Id"
+            })
+        }
+    }
 })
 
 module.exports = taskRouter;
--- express-task-manager/utils/validators/task-validator.js
+++ express-task-manager/utils/validators/task-validator.js
@@ -7,7 +7,28 @@ class TaskValidator {
     }
 
     checkTaskReqBody(task) {
+        const requiredProperties = ['id', 'title', 'description', 'flag'];
+        const actualProperties = Object.keys(task);
 
+        requiredProperties.forEach(prop => {
+            if(!actualProperties.includes(prop)) {
+                return false;
+            }
+        });
+
+        if(
+            typeof task.id !== 'number' || 
+            typeof task.title !== 'string' || 
+            typeof task.description !== 'string' || 
+            typeof task.flag !== 'string'
+        ) {
+            return false;
+        }
+        return true;
+    }
+
+    checkTaskDuplicateId(task, tasks) {
+        return tasks.filter((t) => t.id == task.id).length == 0;
     }
 }
 
