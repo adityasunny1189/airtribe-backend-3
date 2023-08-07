bf7239eca94c765a3e2dd4b4ddb7673dc2b74930
Aditya Pathak <adityapathak1189@gmail.com>
2023-08-07 16:22:05 +0530

    cached data using redis

--- news-api/controllers/v1/userController.js
+++ news-api/controllers/v1/userController.js
@@ -2,7 +2,17 @@ const getNews = require('../../helpers/utils/news');
 const User = require('../../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const redis = require('redis');
 
+const redisClient = redis.createClient();
+
+const connectRedisClient = async () => {
+    await redisClient.connect();
+}
+
+connectRedisClient();
+
+const DEFAULT_EXPIRY_TIME = 3600;
 const BASE_URL = 'https://newsapi.org/v2/top-headlines';
 const SEARCH_BASE_URL = 'https://newsapi.org/v2/everything';
 
@@ -125,37 +135,20 @@ const GetNews = async (req, res) => {
         const country = req?.country || 'in';
         const category = req?.category || 'general';
         const url = `${BASE_URL}?country=${country}&category=${category}&apiKey=${process.env.NEWS_API_KEY}`;
-        try {
-            let news = await getNews(url);
-            res.status(200).send(news);
-        } catch (err) {
-            res.status(500).send(err);
-        }
-    } else {
-        if(req.message) {
-            res.status(401).send({
-                message: req.message
-            })
-        } else {
-            res.status(500).send({
-                message: "invalid token"
-            })
-        }
-    }
-}
-
-// or
-
-const GetNewsPromise = (req, res) => {
-    if(req.user) {
-        const country = req?.country || 'in';
-        const category = req?.category || 'general';
-        const url = `${BASE_URL}?country=${country}&category=${category}&apiKey=${process.env.NEWS_API_KEY}`;
-        let news =  getNews(url);
-        news.then(data => {
-            res.status(200).send(news);
+        redisClient.get('news').then(async data => {
+            if(data) {
+                res.status(200).send(JSON.parse(data));
+            } else {
+                try {
+                    let news = await getNews(url);
+                    redisClient.setEx('news', DEFAULT_EXPIRY_TIME, JSON.stringify(news));
+                    res.status(200).send(news);
+                } catch (err) {
+                    res.status(500).send(err);
+                }
+            }
         }).catch(err => {
-            res.status(500).send(err);
+            console.log(err);
         })
     } else {
         if(req.message) {
@@ -170,17 +163,53 @@ const GetNewsPromise = (req, res) => {
     }
 }
 
+// NOTE: Or use this without async await, here the value returned is a promise while when we use async await then the resolved or rejected value is directly returned
+
+// const GetNewsPromise = (req, res) => {
+//     if(req.user) {
+//         const country = req?.country || 'in';
+//         const category = req?.category || 'general';
+//         const url = `${BASE_URL}?country=${country}&category=${category}&apiKey=${process.env.NEWS_API_KEY}`;
+//         let news =  getNews(url);
+//         news.then(data => {
+//             res.status(200).send(news);
+//         }).catch(err => {
+//             res.status(500).send(err);
+//         })
+//     } else {
+//         if(req.message) {
+//             res.status(401).send({
+//                 message: req.message
+//             })
+//         } else {
+//             res.status(500).send({
+//                 message: "invalid token"
+//             })
+//         }
+//     }
+// }
+
 const SearchNews = async (req, res) => {
+    let keyword = req.params.keyword;
     if(req.user) {
-        const url = `${SEARCH_BASE_URL}?q=${req.params.keyword}&apiKey=${process.env.NEWS_API_KEY}`;
-        try {
-            let news = await getNews(url);
-            res.status(200).send(news);
-        } catch (error) {
-            res.status(500).send({
-                message: error
-            });
-        }
+        const url = `${SEARCH_BASE_URL}?q=${keyword}&apiKey=${process.env.NEWS_API_KEY}`;
+        redisClient.get(keyword).then(async data => {
+            if(data) {
+                res.status(200).send(JSON.parse(data));
+            } else {
+                try {
+                    let news = await getNews(url);
+                    redisClient.setEx(keyword, DEFAULT_EXPIRY_TIME, JSON.stringify(news));
+                    res.status(200).send(news);
+                } catch (error) {
+                    res.status(500).send({
+                        message: error
+                    });
+                }
+            }
+        }).catch(err => {
+            console.log(err);
+        })
     } else {
         if(req.message) {
             res.status(401).send({
