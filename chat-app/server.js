095c14e93ff164f53144c2b4e02504e9fc0c8be3
Aditya Pathak <adityapathak1189@gmail.com>
2023-09-03 23:08:06 +0530

    initialised the project

---
+++ chat-app/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const PORT = 3001;
+
+const app = express();
+const httpServer = http.createServer(app);
+const server = socketio(httpServer);
+
+server.on('connection', (socket) => {
+    console.log("A user connected");
+
+    socket.on('joinRoom', ({ username, roomId }) => {
+        
+    });
+
+    socket.on('sendMessage', () => {
+
+    });
+
+    socket.on('disconnect', () => {
+
+    });
+});
+
+
+server.listen(PORT, (err) => {
+    if(err) {
+        console.log("Error while running the server ", err);
+        return;
+    }
+    console.log("Server up and running at port: ", PORT);
+});
