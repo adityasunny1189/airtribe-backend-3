ee86cb765f4615f54033e3e7fdc3f106c902cd11
Aditya Pathak <adityapathak1189@gmail.com>
2023-09-04 16:06:07 +0530

    chat app

--- chat-app/server.js
+++ chat-app/server.js
@@ -2,26 +2,57 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 
-const PORT = 5000;
+const PORT = 3001;
 
 const app = express();
 const httpServer = http.createServer(app);
 const server = socketio(httpServer);
 
+let users = [];
+
+const addUser = (socketId, username, roomId) => {
+    let newUser = {
+        socketId: socketId,
+        username: username,
+        roomId: roomId
+    };
+    return newUser;
+}
+
 server.on('connection', (socket) => {
     console.log("A user connected");
 
     socket.on('joinRoom', ({ username, roomId }) => {
+        let user = addUser(socket.id, username, roomId);
 
+        users.push(user);
+        
+        socket
+            .join(user.roomId);
+
+        socket
+            .emit('message', 'Welcome to airtribe chat');
+        
+        socket
+            .broadcast
+            .to(roomId)
+            .emit('message', `${username} joined the chat`);
     });
 
-    socket.on('sendMessage', () => {
-
+    socket.on('sendMessage', (message) => {
+        let user = getUser(socket.id);
+        if(user) {
+            io.to(user.roomId).emit('message', message);
+        }
     });
 
     socket.on('disconnect', () => {
-
+        let user = getUser(socket.id);
+        if(user) {
+            io.to(user.roomId).emit('message', `${user.username} has left the room`);
+        }
     });
+    
 });
 
 
